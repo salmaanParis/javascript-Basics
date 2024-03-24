628c4c7674351f0856f0a575bd276a0cc2a33f51
salmaanParis <youtonez@gmail.com>
2024-03-24 23:33:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('hello');
+//Variables
+var a=9;
+console.log(a);
+var a=23;
+console
+/// Let 
+let b=80;
+console.log(b);
+{
+    let b=70;
+    console.log(b)
+}
+///
+var name='miki';
+console.log(typeof(nam));
